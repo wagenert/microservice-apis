2c6dd2462e9c540f9ed9c2af2f5b991b1f6df16f
Thomas Wagener <thomas.wagener@gls-group.com>
2024-07-12 18:24:19 +0200

    up to 2.6

---
+++ my_code/orders/api/schemas.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel, Field, conlist, conint, field_validator
+
+
+class Size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class Status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+
+    @field_validator('quantity')
+    def quantity_non_nullable(cls, value):
+        assert value is not None, 'quantity may not be None'
+        return value
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_length=1)
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
